a3985a8bbdd98b6b3d3ff4f7a65492c17f9ead4e
TDR_Minecraft <tdr.minecraft@gmail.com>
2020-08-09 12:51:42 +0200

    Add 2 Classes
    JoinEvent
    MainCalss

---
+++ src/main/java/de/tdrstudios/spawn/JoinEvent.java
@@ -0,0 +1,19 @@
+package de.tdrstudios.spawn;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class JoinEvent implements Listener {
+
+    public void onJoin(PlayerJoinEvent e) {
+
+        Player p = e.getPlayer();
+
+
+
+
+    }
+
+}
--- src/main/java/de/tdrstudios/spawn/SpawnPlugin.java
+++ src/main/java/de/tdrstudios/spawn/SpawnPlugin.java
@@ -1,4 +1,21 @@
 package de.tdrstudios.spawn;
 
-public class SpawnPlugin e{
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SpawnPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+        System.out.println("[TDRStudios] Spawn Pl wird geladen!");
+        this.getServer().getPluginManager().registerEvents(new JoinEvent(), this);
+    }
+
+    @Override
+    public void onDisable() {
+
+
+    }
+
+
 }
