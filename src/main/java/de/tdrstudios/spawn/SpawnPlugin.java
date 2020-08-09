23f54ce7bcb1437f1eea63433144badabc8e1a38
TDR_Minecraft <tdr.minecraft@gmail.com>
2020-08-09 13:00:26 +0200

    Update MainClass

--- src/main/java/de/tdrstudios/spawn/SpawnPlugin.java
+++ src/main/java/de/tdrstudios/spawn/SpawnPlugin.java
@@ -1,19 +1,36 @@
 package de.tdrstudios.spawn;
 
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public class SpawnPlugin extends JavaPlugin {
+
+    public static File ConfigFile;
+    FileConfiguration config = this.getConfig();
+    Location dummyloc;
 
     @Override
     public void onEnable() {
 
+
+
         System.out.println("[TDRStudios] Spawn Pl wird geladen!");
         this.getServer().getPluginManager().registerEvents(new JoinEvent(), this);
+        initConfig();
     }
 
     @Override
     public void onDisable() {
 
+
+    }
+
+    public void initConfig() {
+        config.addDefault("Spawnpoint: ", dummyloc);
+        this.saveDefaultConfig();
 
     }
 
